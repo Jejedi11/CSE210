39cb83c90ac5d991ed2e460bef64c11c7e515dd3
James Michelson <jejedi110@gmail.com>
2025-10-30 13:42:19 -0700

    Project 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        int x = 5;
+        Console.Write(x);
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Enter your name");
+        string name = Console.ReadLine();
+
+        if (name == "James") 
+        {
+            Console.WriteLine($"Your name is {name}");
+        }
+        
     }
 }
