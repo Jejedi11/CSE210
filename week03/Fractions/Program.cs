482cbf187072abfcc9b65d75f1f0dda06405ea1d
James Michelson <jejedi110@gmail.com>
2025-11-12 12:18:08 -0700

    Fractions project

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction = new Fraction(5);
+
+        fraction.SetBottom(10);
+        fraction.SetTop(6);
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine(fraction.GetFractionalString());
     }
 }
