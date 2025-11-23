ff7a367f125847c406bab2aa18a3df7dbdb8a729
James Michelson <jejedi110@pm.me>
2025-11-22 22:14:31 -0700

    YouTube video project

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -6,33 +6,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
-        List<Comment> makeComment(string name, string text)
+
+        List<Comment> makeComment(string name1, string text1, string name2, string text2, string name3, string text3, string name4, string text4)
         {
             List<Comment> comments = new List<Comment>();
-            for (int i = 0; i < 3; i++)
-        {
-            Comment comment = new Comment(name, text);
-            comments.Add(comment);
+            Comment comment1 = new Comment(name1, text1);
+            comments.Add(comment1);
+            Comment comment2 = new Comment(name2, text2);
+            comments.Add(comment2);
+            Comment comment3 = new Comment(name3, text3);
+            comments.Add(comment3);
+            Comment comment4 = new Comment(name4, text4);
+            comments.Add(comment4);
             return comments;
         }
-        }
-
-        List<Comment> makeCommentList()
-        {
-            makeComment
-        }
-        
 
         List<Video> videos = new List<Video>();
         void makeVideo(string title, string author, double length, List<Comment> comments)
         {
-            for (int i = 0; i < 4; i++)
-        {
             Video video = new Video(title, author, length, comments);
             videos.Add(video);
         }
-        }
 
-        makeVideo("Summer Cabin", "Bob Ross", 10.3, makeComment("Leonard", "I like this")); 
+        makeVideo("Summer Cabin", "Bob Ross", 10.3, makeComment("Leonard", "I like this", "Stephen", "Wow, how does that work?", "Miriam", "I want to try this!", "Charles", "Nice Work!"));
+        makeVideo("Boogie", "Disco Fred", 3.22, makeComment("Leonard", "I like this", "Stephen", "Wow, how does that work?", "Miriam", "I want to try this!", "Charles", "Nice Work!"));
+        makeVideo("Piano Solo", "Han Piano", 4.10, makeComment("Leonard", "I like this", "Stephen", "Wow, how does that work?", "Miriam", "I want to try this!", "Charles", "Nice Work!"));
+        makeVideo("Making a Living", "Charlie Chaplin", 8.30, makeComment("Leonard", "I like this", "Stephen", "Wow, how does that work?", "Miriam", "I want to try this!", "Charles", "Nice Work!"));
+        foreach (Video video in videos)
+        {
+            video.Display();
+        }
     }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Transactions;
 
 public class Video
@@ -9,7 +10,7 @@ public class Video
 
     public int ListComments()
     {
-        return _comments.Count;
+        return _comments.Count();
     }
 
     public Video(string title, string author, double length, List<Comment> comments)
@@ -18,5 +19,14 @@ public class Video
         _author = author;
         _length = length;
         _comments = comments;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Video Length: {_length}");
+        Console.WriteLine($"Comments: {ListComments()}");
+
     }
 }
